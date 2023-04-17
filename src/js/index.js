7483e5374ce4575bf1b272b461514577d72f44d8
Artemiks24 <pff.artyom@yandex.ru>
2023-04-17 12:06:31 +0300

    init: add crock solution

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,9 @@ import '../scss/style.scss'
 import { showMore } from './show-hide.js'
 import { translate } from './interactive.js'
 import { swiperStart } from './sliders'
+import { escTranslate } from './interactive.js'
 
 swiperStart()
 translate()
 showMore()
-
+escTranslate()
--- src/js/interactive.js
+++ src/js/interactive.js
@@ -1,3 +1,4 @@
+
 const wrapper = document.querySelector('.wrapper')
 const aside = document.querySelector('.aside')
 const main = document.querySelector('.main')
@@ -14,47 +15,63 @@ export function translate() {
       aside.classList.add('aside--translate')
       sections.forEach((e) => e.classList.add('blur'))
       main.classList.add('blur')
+      document.body.style.overflow = 'hidden';
     }
     if (e.target.closest('.button--hide-aside')) {
       aside.classList.remove('aside--translate')
       sections.forEach((e) => e.classList.remove('blur'))
       main.classList.remove('blur')
+      
     }
     if (e.target.closest('.button--show-feedback')) {
-      feedback.classList.add('feedback--translate')
       aside.classList.remove('aside--translate')
+      feedback.classList.add('feedback--translate')
       sections.forEach((e) => e.classList.add('blur'))
-      aside.classList.add('blur')
       main.classList.add('blur')
+      document.body.style.overflow = 'hidden';
     }
     if (e.target.closest('.button--hide-feedback')) {
       feedback.classList.remove('feedback--translate')
-      aside.classList.remove('blur')
       main.classList.remove('blur')
       sections.forEach((e) => e.classList.remove('blur'))
+      document.body.style.overflow = 'visible';
     }
     if (e.target.closest('.button--show-call')) {
       aside.classList.remove('aside--translate')
       call.classList.add('call--translate')
       sections.forEach((e) => e.classList.add('blur'))
-      aside.classList.add('blur')
       main.classList.add('blur')
+      document.body.style.overflow = 'hidden';
     }
     if (e.target.closest('.button--hide-call')) {
+      if (!e.target) return
       call.classList.remove('call--translate')
-      aside.classList.remove('blur')
       main.classList.remove('blur')
       sections.forEach((e) => e.classList.remove('blur'))
+      document.body.style.overflow = 'visible';
     }
 
     if (e.target.closest('.main')) {
       if (document.documentElement.clientWidth > 768) {
-        if (aside.classList.contains('aside--translate')) {
-          aside.classList.remove('aside--translate')
-          sections.forEach((e) => e.classList.remove('blur'))
-          main.classList.remove('blur')
-        }
+        aside.classList.remove('aside--translate')
+        call.classList.remove('call--translate')
+        feedback.classList.remove('feedback--translate')
+        sections.forEach((e) => e.classList.remove('blur'))
+        main.classList.remove('blur')
+        document.body.style.overflow = 'visible';
       }
+    }
+  })
+}
+
+export function escTranslate() {
+  document.addEventListener ('keydown', (e) => {
+    if(e.keyCode === 27) {
+      call.classList.remove('call--translate')
+      feedback.classList.remove('feedback--translate')
+      sections.forEach((e) => e.classList.remove('blur'))
+      main.classList.remove('blur')
+      document.body.style.overflow = 'visible';
     }
   })
 }
